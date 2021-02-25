164ec06ab321bd3b3aed5cfcb16ff0958f799c7c
Black Pearl <arraa.marie@gmail.com>
2021-02-25 16:49:06 -0600

    Working API and weather dashboard :)

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
     function searchWeather(searchValue) {
         $.ajax({
             type: "GET",
-            url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial",
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=5ac1c736110842ead64df0939354907a&units=imperial",
             dataType: "json",
             success: function (data) {
                 // create history link for this search
@@ -59,7 +59,7 @@ $(document).ready(function () {
     function getForecast(searchValue) {
         $.ajax({
             type: "GET",
-            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial",
+            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=5ac1c736110842ead64df0939354907a&units=imperial",
             dataType: "json",
             success: function (data) {
                 // overwrite any existing content with title and empty row
@@ -93,7 +93,7 @@ $(document).ready(function () {
     function getUVIndex(lat, lon) {
         $.ajax({
             type: "GET",
-            url: "http://api.openweathermap.org/data/2.5/uvi?appid=7ba67ac190f85fdba2e2dc6b9d32e93c&lat=" + lat + "&lon=" + lon,
+            url: "http://api.openweathermap.org/data/2.5/uvi?appid=5ac1c736110842ead64df0939354907a&lat=" + lat + "&lon=" + lon,
             dataType: "json",
             success: function (data) {
                 var uv = $("<p>").text("UV Index: ");
@@ -115,7 +115,7 @@ $(document).ready(function () {
         });
     }
 
-    // get current history, if any
+    // Grabs current history
     var history = JSON.parse(window.localStorage.getItem("history")) || [];
 
     if (history.length > 0) {
